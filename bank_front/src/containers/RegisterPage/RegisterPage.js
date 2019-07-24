eb8a62a4e66edbeeab7a398fb026e1852812ba1d
Dawid Kostrzewa <dawid.ko1997@gmail.com>
2019-07-24 11:14:21 +0200

    register form setting values to state

--- bank_front/src/containers/RegisterPage/RegisterPage.js
+++ bank_front/src/containers/RegisterPage/RegisterPage.js
@@ -2,8 +2,38 @@ import React, { Component } from 'react';
 import './RegisterPage.scss';
 import heroImg from '../../assets/images/bank-hero-nooverlay.png';
 import { Link } from 'react-router-dom';
+import { DatePicker } from 'react-materialize';
 
 class RegisterPage extends Component {
+
+  state = {
+    loginVal: null,
+    passwordVal: null,
+    emailVal: null,
+    nameVal: null,
+    surnameVal: null,
+    birthdayVal: null,
+  };
+
+  componentDidMount() {
+    /*  const elems = document.querySelectorAll('.datepicker');
+      const instances = M.Datepicker.init(elems, options);*/
+  }
+
+  handleInputChange = (e, field) => {
+    this.setState({
+      ...this.state,
+      [field]:e.target.value
+    })
+
+  };
+
+  handleDate = (date) => {
+    this.setState({
+      ...this.state,
+      birthdayVal:date
+    })
+  };
 
   render() {
 
@@ -12,6 +42,11 @@ class RegisterPage extends Component {
       rgba(3, 94, 230, 0.9), 
       rgba(3, 94, 230, 0.5)
     ),url(${heroImg}) `,
+    };
+
+    const options = {
+      minDate: new Date(1980, 1, 1),
+      yearRange: 80,
     };
 
     return (
@@ -24,14 +59,15 @@ class RegisterPage extends Component {
             <form className="content__form">
               <div className="content__form__form">
                 <div className="content__form__form__account-info">
-                  <input type="text" placeholder="Login"/>
-                  <input type="password" placeholder="Password"/>
-                  <input type="email" placeholder="Email"/>
+                  <input type="text" placeholder="Login" onChange={(event) => this.handleInputChange(event, 'loginVal')}/>
+                  <input type="password" placeholder="Password" onChange={(event) => this.handleInputChange(event, 'passwordVal')}/>
+                  <input type="email" placeholder="Email" onChange={(event) => this.handleInputChange(event, 'emailVal')}/>
                 </div>
                 <div className="content__form__form__user-info">
-                  <input type="text" placeholder="Name"/>
-                  <input type="text" placeholder="Surname"/>
-                  <input type="text" placeholder="Birthday"/>
+                  <input type="text" placeholder="Name" onChange={(event) => this.handleInputChange(event, 'nameVal')}/>
+                  <input type="text" placeholder="Surname" onChange={(event) => this.handleInputChange(event, 'surnameVal')}/>
+                  <DatePicker placeholder="Birthday" onChange={(date) => this.handleDate(date)} options={options}/>
+
                 </div>
               </div>
               <div className="content__form__register-btn">
