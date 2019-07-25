2f5e04e79b30347d32fb77fd8c8d2d9db7970168
Dawid Kostrzewa <dawid.ko1997@gmail.com>
2019-07-25 10:00:44 +0200

    moved register form to other component

--- bank_front/src/containers/RegisterPage/RegisterPage.js
+++ bank_front/src/containers/RegisterPage/RegisterPage.js
@@ -8,6 +8,7 @@ import { ErrorMessage, Field, Form, Formik } from 'formik';
 import axios from 'axios';
 import { API_URL } from '../../utils/api';
 import RegisteredUser from './RegisteredUserInfo/RegisteredUser';
+import RegisterForm from './RegisterForm/RegisterForm';
 
 class RegisterPage extends Component {
 
@@ -21,14 +22,14 @@ class RegisterPage extends Component {
       birthdayVal: null,
     },
     error: null,
-    newUserData:{
-      login:'',
-      email:'',
-      name:'',
-      surname:''
+    newUserData: {
+      login: '',
+      email: '',
+      name: '',
+      surname: '',
     },
 
-    isRegisterSuccess:false
+    isRegisterSuccess: false,
 
   };
 
@@ -37,17 +38,17 @@ class RegisterPage extends Component {
       const instances = M.Datepicker.init(elems, options);*/
   }
 
- /* handleInputChange = (e, field) => {
-    this.setState({
-      ...this.state,
-      form: {
-        ...this.state.form,
-        [field]: e.target.value,
-      },
+  /* handleInputChange = (e, field) => {
+     this.setState({
+       ...this.state,
+       form: {
+         ...this.state.form,
+         [field]: e.target.value,
+       },
 
-    });
+     });
 
-  };*/
+   };*/
 
   handleDate = (date) => {
     this.setState({
@@ -60,8 +61,24 @@ class RegisterPage extends Component {
     });
   };
 
-  registerUser = (data) => {
-
+  registerUser = (values, actions) => {
+    axios.post(`${API_URL}/register`, values)
+      .then(res => {
+        console.log(res.data);
+        actions.setSubmitting(false);
+        this.setState({
+          isRegisterSuccess: true,
+          newUserData: res.data,
+        });
+      })
+      .catch(err => {
+        console.log(err.response.data);
+        this.setState({
+          error: err.response.data.error,
+        });
+        actions.setSubmitting(false);
+      });
+    console.log(values);
   };
 
   render() {
@@ -73,106 +90,10 @@ class RegisterPage extends Component {
     ),url(${heroImg}) `,
     };
 
-    const options = {
-      minDate: new Date(1980, 1, 1),
-      yearRange: 80,
-    };
 
-    const displayForm = this.state.isRegisterSuccess?<RegisteredUser user={this.state.newUserData}/>: <Formik
-      initialValues={{
-        login: '',
-        password: '',
-        email: '',
-        name: '',
-        surname: '',
-      }}
-      validate={values => {
-        let errors = {};
-        if (!values.email) {
-          errors.email = 'Email is required';
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-          errors.email = 'Invalid email address';
-        }
-
-        if (!values.login) {
-          errors.login = 'Login is Required';
-        }
-
-        if (!values.password) {
-          errors.password = 'Password is Required';
-        }
-        if (!values.name) {
-          errors.name = 'Name is Required';
-        }
-
-        if (!values.surname) {
-          errors.surname = 'Surname is Required';
-        }
-
-        return errors;
-      }}
-      onSubmit={(values, actions) => {
-        //this.registerUser(values);
-        axios.post(`${API_URL}/register`, values)
-          .then(res => {
-            console.log(res.data);
-            actions.setSubmitting(false);
-            this.setState({
-              isRegisterSuccess:true,
-              newUserData:res.data
-            })
-          })
-          .catch(err => {
-            console.log(err.response.data);
-            this.setState({
-              error: err.response.data.error,
-            });
-            actions.setSubmitting(false);
-          });
-        console.log(values);
-      }}
-      render={({
-                 values,
-                 errors,
-                 status,
-                 touched,
-                 handleBlur,
-                 handleChange,
-                 handleSubmit,
-                 isSubmitting,
-               }) => (
-        <form onSubmit={handleSubmit} className="content__form">
-          <div className="content__form__form">
-            <div className="content__form__form__account-info">
-              <input type="text" name="login" className="error-message" value={values.login} placeholder="Login"
-                     onChange={handleChange}/>
-              <ErrorMessage component="span" className="error-message" name="login"/>
-              <input type="password" name="password" value={values.password} placeholder="Password"
-                     onChange={handleChange}/>
-              <ErrorMessage component="span" className="error-message" name="password"/>
-              <input type="text" name="email" value={values.email} placeholder="Email"
-                     onChange={handleChange}/>
-              <ErrorMessage component="span" className="error-message" name="email"/>
-            </div>
-            <div className="content__form__form__user-info">
-              <input type="text" name="name" value={values.name} placeholder="Name" onChange={handleChange}/>
-              <ErrorMessage component="span" className="error-message" name="name"/>
-              <input type="text" name="surname" value={values.surname} placeholder="Surname"
-                     onChange={handleChange}/>
-              <ErrorMessage component="span" className="error-message" name="surname"/>
-              <DatePicker placeholder="Birthday" onChange={(date) => this.handleDate(date)} options={options}/>
-            </div>
-          </div>
-          <div className="content__form__error">
-            {this.state.error}
-          </div>
-          <div className="content__form__register-btn">
-
-            <Button disabled={isSubmitting}>Register</Button>
-          </div>
-        </form>
-      )}
-    />
+    const displayForm = this.state.isRegisterSuccess ? <RegisteredUser user={this.state.newUserData}/> :
+      <RegisterForm register={(values, actions) => this.registerUser(values, actions)}
+                    birthdayField={(date) => this.handleDate(date)} error={this.state.error}/>;
 
 
     return (
