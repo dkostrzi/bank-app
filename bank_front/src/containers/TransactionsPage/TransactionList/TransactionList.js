c91919ed08b63cc3d61c4fe7b5f0cc9bd3136228
Dawid Kostrzewa <dawid.ko1997@gmail.com>
2019-08-05 13:44:30 +0200

    added search input to transaction list

--- bank_front/src/containers/TransactionsPage/TransactionList/TransactionList.js
+++ bank_front/src/containers/TransactionsPage/TransactionList/TransactionList.js
@@ -1,68 +1,91 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Moment from 'react-moment';
 
-const transactionList = (props)=>{
-  let lp = 0;
-  return(
-    <div className="TransactionsDashboard__data">
-      <h3>Transactions</h3>
-      <table className="responsive-table highlight">
-        <thead>
-        <tr>
-          <th>No.</th>
-          <th>User</th>
-          <th>Bill</th>
-          <th>Date</th>
-          <th>Amount money</th>
-          <th>Transfer title</th>
-        </tr>
-        </thead>
+class TransactionList extends Component {
 
-        <tbody>
-        {props.transaction.map(item => {
+  state = {
+    searchValue: '',
+  };
 
-          const ifSender = item.id_sender==props.userId?true:false;
-          const color = ifSender?"red":"green";
-          const style = {
-            color:color
-          };
+  handleSearchChange = (e) => {
+    this.setState({
+      ...this.state,
+      searchValue: e.target.value,
+    });
+  };
 
+  render() {
+    let lp = 0;
+    return (
+      <div className="TransactionsDashboard__data">
+        <h3>Transactions</h3>
+        <input type="text" placeholder="Search" value={this.state.searchValue} onChange={this.handleSearchChange}/>
+        <table className="responsive-table highlight">
+          <thead>
+          <tr>
+            <th>No.</th>
+            <th>User</th>
+            <th>Bill</th>
+            <th>Date</th>
+            <th>Amount money</th>
+            <th>Transfer title</th>
+          </tr>
+          </thead>
 
-          lp++;
-          if(ifSender){
-            return (<tr style={style} key={item.id}>
-              <td>{lp}</td>
-              <td>{item.getRecipientdata.name} {item.getRecipientdata.surname}</td>
-              <td>{item.getRecipientdata.bills[0].account_bill} </td>
-              {/*<td>{new Date(item.date_time).toTimeString()}</td>*/}
-              <td><Moment format="YYYY-MM-DD HH:mm">
-                {new Date(item.date_time)}
-              </Moment></td>
-              <td>-{item.amount_money.toFixed(2)} $</td>
-              <td>{item.transfer_title}</td>
-            </tr>)
-          }
-          else{
-            return (<tr style={style} key={item.id}>
-              <td>{lp}</td>
-              <td>{item.getSenderdata.name} {item.getSenderdata.surname}</td>
-              <td>{item.getSenderdata.bills[0].account_bill} </td>
-              <td><Moment format="YYYY-MM-DD HH:mm">
-                {new Date(item.date_time)}
-              </Moment></td>
-              <td>{item.amount_money.toFixed(2)} $</td>
-              <td>{item.transfer_title}</td>
-            </tr>)
-          }
+          <tbody>
+          {this.props.transaction
+            .filter(item => {
+              const ifSender = item.id_sender == this.props.userId ? true : false;
+              const phrase = ifSender?`${item.getRecipientdata.name} ${item.getRecipientdata.surname}`:`${item.getSenderdata.name} ${item.getSenderdata.surname}`;
+              const accountBill = ifSender?item.getRecipientdata.bills[0].account_bill:item.getSenderdata.bills[0].account_bill
+              return phrase.toLowerCase().includes(this.state.searchValue.toLowerCase()) || accountBill.includes(this.state.searchValue);
+            })
+            .map(item => {
+
+              const ifSender = item.id_sender == this.props.userId ? true : false;
+              const color = ifSender ? 'red' : 'green';
+              const style = {
+                color: color,
+              };
 
 
-        })}
-        </tbody>
-      </table>
+              lp++;
+              if (ifSender) {
+                return (<tr style={style} key={item.id}>
+                  <td>{lp}</td>
+                  <td>{item.getRecipientdata.name} {item.getRecipientdata.surname}</td>
+                  <td>{item.getRecipientdata.bills[0].account_bill} </td>
+                  {/*<td>{new Date(item.date_time).toTimeString()}</td>*/}
+                  <td><Moment format="YYYY-MM-DD HH:mm">
+                    {new Date(item.date_time)}
+                  </Moment></td>
+                  <td>-{item.amount_money.toFixed(2)} $</td>
+                  <td>{item.transfer_title}</td>
+                </tr>);
+              } else {
+                return (<tr style={style} key={item.id}>
+                  <td>{lp}</td>
+                  <td>{item.getSenderdata.name} {item.getSenderdata.surname}</td>
+                  <td>{item.getSenderdata.bills[0].account_bill} </td>
+                  <td><Moment format="YYYY-MM-DD HH:mm">
+                    {new Date(item.date_time)}
+                  </Moment></td>
+                  <td>{item.amount_money.toFixed(2)} $</td>
+                  <td>{item.transfer_title}</td>
+                </tr>);
+              }
 
 
-    </div>
-  )
+            })}
+          </tbody>
+        </table>
+
+
+      </div>
+    );
+  }
+
+
 }
 
-export default transactionList;
+export default TransactionList;
